e7fec978b5c87fcb4ed563a120d2e70160d4e6fe
Conor Curran <conor@forwind.net>
2013-10-21 01:31:04 +0100

    update the current release link

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -19,7 +19,7 @@ class ReleasesController < ApplicationController
   end
 
   def current
-    @release = Release.find_by_cat('FWD09')
+    @release = Release.find_by_cat('FWD10')
     set_sharing_details(share_text(), "http://www.forwind.net/releases/" + @release.cat.downcase, @release.title)
     render :show
   end
