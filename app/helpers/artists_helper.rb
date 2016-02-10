38e457791ba1842c2f7c840499369b75ca1a8352
Conor Curran <conor@forwind.net>
2016-02-10 00:38:57 +0000

    use simple regex

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -7,10 +7,8 @@ module ArtistsHelper
   def external_link_trimmer(link)
     r = link.split('www.')[1]
     slashes = r.split('/')
-    if slashes[0] == 'soundcloud.com' or slashes[0] == 'myspace.com'
+    if slashes[0].match(/forwind|soundcloud/)
       return slashes[0] + '/' + slashes[1]
-    elsif slashes[0] == 'last.fm'
-      return slashes[0] + '/' + slashes[1] + '/' + slashes[2]
     else
       return slashes[0]
     end
