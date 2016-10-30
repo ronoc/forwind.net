acf220e8f890ee02f6bc1859fdc2868de80ecd6e
Conor Curran <conor@forwind.net>
2016-10-30 23:51:01 +0000

    use redis to set the index banner image and link, i.e. let's try to cut down on deployments.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,10 @@
 module HomeHelper
   def blog_formatting(body)
   end
+
+  def banner
+    bannerLink = $redis.get("bannerLink")
+    bannerImage = File.join(@s3_images, "index", $redis.get("bannerImage"))
+    return "<a href=#{bannerLink}><img src=#{bannerImage}></a>"
+  end
 end
