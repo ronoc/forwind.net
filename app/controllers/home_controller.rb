3fb64b8c31b3b065ed25312e261f0d0191327f6c
ramonjd <ramonjd@gmail.com>
2018-01-19 23:43:57 +1100

    starting responsive stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,18 @@
 class HomeController < ApplicationController
   layout 'base'
-  before_action :random_promo
+  #before_action :random_promo
   
-  def random_promo
-    featured = $redis.lrange("featuredReleases", 0, $redis.llen("releases")-1)
-    @small_releases = Release.where("cat IN (?)", featured)
-                             .order('random()').to_a                             
-    puts "small releases " + @small_releases.to_s
-    pds = Podcast.pluck(:id)
-    @podcast = Podcast.find(pds.sample)
-    @context = ""
-    #@blogs = BlogPost.all(:limit => 6, :conditions => ["published_at IS NOT NULL"], :order => "published_at")
-    #                 .reject{|b| b.tags.include?("podcasts")}
-    #                 .first(3)x
-  end
+  # def random_promo
+  #   featured = $redis.lrange("featuredReleases", 0, $redis.llen("releases")-1)
+  #   @small_releases = Release.where("cat IN (?)", featured)
+  #                            .order('random()').to_a                             
+  #   puts "small releases " + @small_releases.to_s
+  #   pds = Podcast.pluck(:id)
+  #   @podcast = Podcast.find(pds.sample)
+  #   @context = ""
+  #   #@blogs = BlogPost.all(:limit => 6, :conditions => ["published_at IS NOT NULL"], :order => "published_at")
+  #   #                 .reject{|b| b.tags.include?("podcasts")}
+  #   #                 .first(3)x
+  # end
 end
 
