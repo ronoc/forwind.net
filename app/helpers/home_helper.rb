8423da94c1d06947cbbd3f7394c898ddfa47f582
Conor Curran <conor@forwind.net>
2018-10-29 16:04:30 +0000

    use redis for the main page banner

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,9 +4,6 @@ module HomeHelper
     bannerLink = $redis.get("bannerLink")
     bannerImage = File.join(@s3_images, "index", $redis.get("bannerImage"))
     <<~HEREDOC
-      <h2 class="heading heading--floating-button">
-        <a href="#{bannerLink}">Release</a>
-      </h2>
       <figure>
         <a href="#{bannerLink}"><img alt="" src="#{bannerImage}"></a>
       </figure>
