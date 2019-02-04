068e74641afa98b31d8b10044e8da9f007dfe166
Conor Curran <conor@forwind.net>
2019-02-04 13:29:18 +0000

    tidy up link trimmer, it never worked properly

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,14 +5,14 @@ module ArtistsHelper
   end
 
   def external_link_trimmer(link)
-    r = link.split('www.')[1]
-    slashes = r.split('/')
-    if slashes[0].match(/forwind|soundcloud/)
-      return slashes[0] + '/' + slashes[1]
-    else
-      return slashes[0]
+    r = link.split('.')
+    v = r[r.length - 2]
+    if v.match(/(com|co|org)/)
+      v = r[r.length - 3]
     end
+    v.capitalize
   end
+
   def small_artists_image(image)
     elements = image.split('.')
     elements[0] = elements[0] + '_sm.'
